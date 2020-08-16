5573b10e47dcc9e08cb575d884e8cb8397033071
seungCheolCha <cscculture95@gmail.com>
2020-08-16 23:35:03 +0900

    초기 커밋.

---
+++ ch02_RedShader/Include.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// C/C++ Header
+#include <iostream>
+
+// Windows runtime header
+#include <Windows.h>
+
+// Direct 3D Headers
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#include "Const.h"
