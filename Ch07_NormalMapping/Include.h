38e79faca16ef97163ddc7c865455c8c6d25097c
seungCheolCha <cscculture95@gmail.com>
2020-08-28 18:48:30 +0900

    노멀 매핑 및 환경 매핑을 공부 및 실습

---
+++ Ch07_NormalMapping/Include.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// C/C++ Header
+#include <iostream>
+
+// Windows runtime header
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+// Direct 3D Headers
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#include "Const.h"
